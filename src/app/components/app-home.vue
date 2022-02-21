c39d14357009099ce61a0935d3b42797a386c497
nhb1998 <849990475@qq.com>
2022-02-21 18:18:25 +0800

    定义应用标志组件（AppLogo）

--- src/app/components/app-home.vue
+++ src/app/components/app-home.vue
@@ -1,3 +1,14 @@
 <template>
   <div>欢迎访问宁皓网：）</div>
+  <AppLogo size="64" color='#123456'/>
 </template>
+
+<script>
+import AppLogo from '@/app/components/app-logo';
+
+export default {
+  components: {
+    AppLogo,
+  },
+};
+</script>
