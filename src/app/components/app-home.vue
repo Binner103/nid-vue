9cb837188383d01a23a6457813c353b3cfb51478
nhb1998 <849990475@qq.com>
2022-02-22 15:41:22 +0800

    清理AppHome 组件

--- src/app/components/app-home.vue
+++ src/app/components/app-home.vue
@@ -1,17 +1,3 @@
 <template>
   <div>欢迎访问宁皓网：）</div>
-  <AppLogo size="64" color="#123456" />
-  <AppIcon size="64" color="#123456" name="comment"/>
 </template>
-
-<script>
-import AppLogo from '@/app/components/app-logo';
-import AppIcon from '@/app/components/app-icon';
-
-export default {
-  components: {
-    AppLogo,
-    AppIcon,
-  },
-};
-</script>
