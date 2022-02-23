8a36c0c0568ae189101c24795698e2dd44a7e3dd
nhb1998 <849990475@qq.com>
2022-02-23 23:41:24 +0800

    创建应用布局组件时设置主题

--- src/app/layout/app-layout.vue
+++ src/app/layout/app-layout.vue
@@ -10,9 +10,10 @@
 
 <script>
 import { defineComponent } from 'vue';
-import {mapGetters} from 'vuex'
+import { mapGetters, mapMutations } from 'vuex';
 import PageHeader from './components/page-header.vue';
 import PageAside from './components/page-aside.vue';
+import { getStorage } from '../app.service';
 
 export default defineComponent({
   name: 'AppLayout',
@@ -34,21 +35,29 @@ export default defineComponent({
    */
   computed: {
     ...mapGetters({
-      theme: 'layout/theme'
-    })
+      theme: 'layout/theme',
+    }),
   },
 
   /**
    * 已创建
    */
   created() {
-    //
+    //主题
+    const theme = getStorage('theme');
+    if (theme) {
+      this.setTheme(theme);
+    }
   },
 
   /**
    * 组件方法
    */
-  methods: {},
+  methods: {
+    ...mapMutations({
+      setTheme: 'layout/setTheme',
+    }),
+  },
 
   /**
    * 使用组件
