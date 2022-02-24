40a30f4128b1c5c0a1899e294660d72b0dfd27ab
nhb1998 <849990475@qq.com>
2022-02-24 21:41:00 +0800

    定义通知提示信息组件（AppNotificationMessage）

--- src/app/notification/app-notification.vue
+++ src/app/notification/app-notification.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="app-notification">AppNotification</div>
+  <div class="app-notification">
+    <AppNotificationMessage
+      v-for="message in messages"
+      :key="message.id"
+      :message="message"
+    />
+  </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import AppNotificationMessage from './components/app-notification-message.vue';
 
 export default defineComponent({
   name: 'AppNotification',
@@ -17,7 +24,24 @@ export default defineComponent({
    * 数据
    */
   data() {
-    return {};
+    return {
+      messages: [
+        {
+          id: 1,
+          content: '空山新雨后, 天气晚来秋',
+          icon: 'cloud',
+        },
+        {
+          id: 2,
+          content: '树树皆秋色, 山山唯落晖',
+          icon: 'landscape',
+        },
+        {
+          id: 3,
+          content: '人烟寒橘柚, 秋色老梧桐',
+        },
+      ],
+    };
   },
 
   /**
@@ -29,7 +53,7 @@ export default defineComponent({
    * 已创建
    */
   created() {
-  // 
+    //
   },
 
   /**
@@ -40,12 +64,14 @@ export default defineComponent({
   /**
    * 使用组件
    */
-  components: {},
+  components: {
+    AppNotificationMessage,
+  },
 });
 </script>
 
 <style scoped>
-.app-notification{
+.app-notification {
   position: fixed;
   bottom: 64px;
   right: 64px;
