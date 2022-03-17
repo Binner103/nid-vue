933a2ad23df75497025a15f08ae5f08ae8b22702
nhb1998 <849990475@qq.com>
2022-03-17 14:06:03 +0800

    用户菜单项目

--- src/user/components/user-menu.vue
+++ src/user/components/user-menu.vue
@@ -2,7 +2,23 @@
   <div class="user-menu">
     <CloseButton @click="$emit('close')" />
     <div class="user-menu-header">
-      <UserName :user="currentUser"/>
+      <UserName :user="currentUser" />
+    </div>
+    <div
+      class="user-menu-items"
+      v-for="(menuItems, menuItemsIndex) in menu"
+      :key="menuItemsIndex"
+    >
+      <div
+        class="user-menu-item"
+        v-for="(item, index) in menuItems"
+        :key="index"
+        @click="$emit('close')"
+      >
+        <router-link class="link" :to="item.linkTo">
+          {{ item.text }}
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -40,6 +56,36 @@ export default defineComponent({
     ...mapGetters({
       currentUser: 'user/currentUser',
     }),
+
+    menu() {
+      return [
+        [
+          {
+            linkTo: {
+              name: 'userPosts',
+              params: { userId: this.currentUser.id },
+            },
+            text: '作品',
+          },
+          {
+            linkTo: {
+              name: 'userComments',
+              params: { userId: this.currentUser.id },
+            },
+            text: '评论',
+          },
+        ],
+        [
+          {
+            linkTo: {
+              name: 'userAccount',
+              params: { userId: this.currentUser.id },
+            },
+            text: '账户',
+          },
+        ],
+      ];
+    },
   },
 
   /**
