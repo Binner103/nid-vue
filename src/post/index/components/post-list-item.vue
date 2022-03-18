a20d07ea687a70d7f222c21a920cbb49e6380c34
nhb1998 <849990475@qq.com>
2022-03-18 17:43:41 +0800

    准备与使用内容列表布局数据

--- src/post/index/components/post-list-item.vue
+++ src/post/index/components/post-list-item.vue
@@ -1,14 +1,15 @@
 <template>
   <div :class="postListItemClasses">
     <PostListItemMedia :item="item" />
-    <PostListItemContent :item="item"/>
+    <PostListItemContent :item="item" />
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import { mapGetters } from 'vuex';
 import PostListItemMedia from './post-list-item-media.vue';
-import PostListItemContent from './post-list-item-content.vue'
+import PostListItemContent from './post-list-item-content.vue';
 
 export default defineComponent({
   props: {
@@ -16,8 +17,11 @@ export default defineComponent({
   },
 
   computed: {
+    ...mapGetters({
+      layout: 'post/index/layout',
+    }),
     postListItemClasses() {
-      return ['post-list-item', this.item.file.orientation];
+      return ['post-list-item', this.item.file.orientation, this.layout];
     },
   },
 
