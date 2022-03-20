f4d5e2e347b965282c4e3a49466c463da925b40b
nhb1998 <849990475@qq.com>
2022-03-20 17:17:31 +0800

    内容页面布局变化

--- src/post/show/post-show.vue
+++ src/post/show/post-show.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-if="showPost">
-    <PostShowMedia :post="post" />
+  <div :class="postShowClasses" v-if="showPost">
+    <PostShowMedia :post="post" @click="onClickPostShowMedia" />
     <h1>{{ post.title }}</h1>
     <div>
       {{ post.content }} - <small>{{ post.user.name }}</small>
@@ -9,9 +9,10 @@
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions, mapMutations } from 'vuex';
 import { defineComponent } from 'vue';
 import PostShowMedia from './components/post-show-media.vue';
+import { getStorage } from '@/app/app.service';
 
 export default defineComponent({
   title() {
@@ -26,23 +27,42 @@ export default defineComponent({
 
   created() {
     this.getPostById(this.postId);
+
+    // 布局
+    const layout = getStorage('post-show-layout');
+
+    if (layout) {
+      this.setLayout(layout);
+    }
   },
 
   computed: {
     ...mapGetters({
       loading: 'post/show/loading',
       post: 'post/show/post',
+      layout: 'post/show/layout',
     }),
 
     showPost() {
       return !this.loading && this.post;
     },
+
+    postShowClasses() {
+      return ['post-show', this.layout];
+    },
   },
 
   methods: {
+    ...mapMutations({
+      setLayout: 'post/show/setLayout',
+    }),
     ...mapActions({
       getPostById: 'post/show/getPostById',
     }),
+
+    onClickPostShowMedia() {
+      this.setLayout(`${this.layout ? '' : 'flow'}`);
+    },
   },
 
   components: {
@@ -50,3 +70,7 @@ export default defineComponent({
   },
 });
 </script>
+
+<style>
+@import './styles/post-show.css';
+</style>
