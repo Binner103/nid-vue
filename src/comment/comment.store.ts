04fa5d1faf97600568a5854289cecbacd84f436e
nhb1998 <849990475@qq.com>
2022-03-24 22:37:25 +0800

    定义评论 Store 模块（commentStoreModule）

---
+++ src/comment/comment.store.ts
@@ -0,0 +1,41 @@
+import { Module } from 'vuex';
+import { RootState } from '@/app/app.store';
+
+export interface CommentStoreState {
+  
+}
+
+export const commentStoreModule: Module<CommentStoreState, RootState> = {
+  /**
+   * 命名空间
+   */
+  namespaced: true,
+
+  /**
+   * 数据
+   */
+  state: {
+  
+  } as CommentStoreState,
+
+  /**
+   * 获取器
+   */
+  getters: {
+   
+  },
+
+  /**
+   * 修改器
+   */
+  mutations: {
+
+  },
+
+  /**
+   * 动作
+   */
+  actions: {
+
+  },
+};
