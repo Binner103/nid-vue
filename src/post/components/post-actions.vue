0024d7470dfb66752e103072ee134a5fbc023624
nhb1998 <849990475@qq.com>
2022-04-12 21:04:25 +0800

    定义内容动作组件（PostActions）

---
+++ src/post/components/post-actions.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="post-actions">
+    <button class="button large" @click="onClickSubmitButton">
+      {{ submitButtonText }}
+    </button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { mapGetters, mapActions, mapMutations } from 'vuex';
+
+export default defineComponent({
+  name: 'PostActions',
+
+  /**
+   * 属性
+   */
+  props: {},
+
+  /**
+   * 事件
+   */
+  emits: ['update', 'create'],
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {
+    ...mapGetters({
+      postId: 'post/create/postId',
+      title: 'post/create/title',
+    }),
+
+    submitButtonText() {
+      return this.postId ? '更新' : '发布';
+    },
+  },
+
+  /**
+   * 已创建
+   */
+  created() {
+    //
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {
+    onClickSubmitButton() {
+      if (!this.title.trim()) return;
+
+      if (this.postId) {
+        this.$emit('update');
+      } else {
+        this.$emit('create');
+      }
+    },
+  },
+
+  /**
+   * 使用组件
+   */
+  components: {},
+});
+</script>
+
+<style scoped>
+@import './styles/post-actions.css';
+</style>
