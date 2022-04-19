2efdb924e813c05725a32743389d5e5d0b5b2ec0
nhb1998 <849990475@qq.com>
2022-04-19 21:27:19 +0800

    定义管理内容列表项目组件（ManagePostListItem）

---
+++ src/manage/post/components/manage-post-list-item.vue
@@ -0,0 +1,57 @@
+<template>
+  <div :class="managePostListItemClasses">managePostListItem</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ManagePostListItem',
+
+  /**
+   * 属性
+   */
+  props: {
+    item: {
+      type: Object,
+    },
+  },
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {
+    managePostListItemClasses() {
+      return ['manage-post-list-item', { selected: false }];
+    },
+  },
+
+  /**
+   * 已创建
+   */
+  created() {
+    //
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {},
+
+  /**
+   * 使用组件
+   */
+  components: {},
+});
+</script>
+
+<style scoped>
+@import './styles/manage-post-list-item.css';
+</style>
