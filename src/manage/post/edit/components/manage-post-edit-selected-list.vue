2785b476312c04afae15bec72e5fc0e635c53cac
nhb1998 <849990475@qq.com>
2022-04-22 15:58:42 +0800

    创建管理内容编辑选择相关组件

---
+++ src/manage/post/edit/components/manage-post-edit-selected-list.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="manage-post-edit-selected-list">
+    <ManagePostEditSelectedListItem />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { mapGetters } from 'vuex';
+import ManagePostEditSelectedListItem from '@/manage/post/edit/components/manage-post-edit-selected-list-item';
+
+export default defineComponent({
+  name: 'ManagePostEditSelectedList',
+
+  /**
+   * 属性
+   */
+  props: {},
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {
+    ...mapGetters({}),
+  },
+
+  /**
+   * 已创建
+   */
+  created() {
+  // 
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {
+  },
+
+  /**
+   * 使用组件
+   */
+  components: {
+    ManagePostEditSelectedListItem,
+  },
+});
+</script>
+
+<style scoped>
+@import './styles/manage-post-edit-selected-list.css';
+</style>
---
+++ src/manage/post/edit/components/manage-post-edit-selected.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="manage-post-edit-selected">
+    <ManageSelectedStatus />
+    <ManagePostEditSelectedList />
+  </div>
+</template>
+
+
+
+<script>
+import { defineComponent } from 'vue';
+import ManageSelectedStatus from '@/manage/components/manage-selected-status';
+import ManagePostEditSelectedList from '@/manage/post/edit/components/manage-post-edit-selected-list';
+
+export default defineComponent({
+  name: 'ManagePostEditSelected',
+
+  /**
+   * 属性
+   */
+  props: {},
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {
+  },
+
+  /**
+   * 已创建
+   */
+  created() {
+  // 
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {
+  },
+
+  /**
+   * 使用组件
+   */
+  components: {
+    ManagePostEditSelectedList,
+    ManageSelectedStatus,
+  },
+});
+</script>
+
+
+
+<style scoped>
+@import './styles/manage-post-edit-selected.css';
+</style>
