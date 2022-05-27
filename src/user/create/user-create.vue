d48c62ff7d1856b1e4fadb874099bbfe6f2cb18c
nhb1998 <849990475@qq.com>
2022-05-27 15:36:49 +0800

    创建登录页面

---
+++ src/user/create/user-create.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="user-create">
+    <div class="from">
+      <h1 class="header">注册用户</h1>
+      <TextField v-model="name" placeholder="用户" />
+      <TextField v-model="password" placeholder="密码" type="password" />
+      <ButtonField text="注册" size="large" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { mapGetters, mapMutations, mapActions } from 'vuex';
+import TextField from '@/app/components/text-field.vue';
+import ButtonField from '@/app/components/button-field.vue';
+
+export default defineComponent({
+  name: 'UserCreate',
+
+  /**
+   * 属性
+   */
+  props: {},
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {
+    ...mapGetters({}),
+  },
+
+  /**
+   * 已创建
+   */
+  created() {
+    //
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {
+    ...mapMutations({}),
+    ...mapActions({}),
+  },
+
+  /**
+   * 使用组件
+   */
+  components: {
+    TextField,
+    ButtonField,
+  },
+});
+</script>
+
+<style scoped>
+@import './styles/user-create.css';
+</style>
